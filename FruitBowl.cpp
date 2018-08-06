9820f43a160c72cc1dbd074c3bf2796112ae6f9a
Stephen Kelly <steveire@gmail.com>
2018-08-06 22:31:03 +0100

    Remove Grape to show the impact on adding it as a module

--- FruitBowl.cpp
+++ FruitBowl.cpp
@@ -2,11 +2,9 @@
 #include "FruitBowl.h"
 
 #include "Apple.h"
-#include "Grape.h"
 
 FruitBowl::FruitBowl() {
   add(std::make_unique<Apple>());
-  add(std::make_unique<Grape>());
 }
 
 std::unique_ptr<AbstractFruit>
